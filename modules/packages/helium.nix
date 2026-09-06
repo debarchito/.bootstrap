13214db7a107968cf7b7a7a8c383d57d0bdd8aa3
Debarchito Nath <debarchiton@proton.me>
2026-09-06 15:35:34 +0530

    chore(helium): update to helium 0.16.5.1

--- modules/packages/helium.nix
+++ modules/packages/helium.nix
@@ -7,15 +7,15 @@
       ...
     }:
     let
-      version = "0.16.4.1";
+      version = "0.16.5.1";
       sources = {
         "x86_64-linux" = {
           url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-          hash = "sha256-z0OoKmW49F/2F3mxjZlyJsTY45keyZJAj4pjoGBjBO8=";
+          hash = "sha256-N6+wwg46ufsbCqEJv/WpTWDCnI3tnFt58cG6TsGxXew=";
         };
         "aarch64-linux" = {
           url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-arm64.AppImage";
-          hash = "sha256-PNDXow9Q4cP5ZW3hXkWDKikXbVEgSDf8MU+IucjMTeo=";
+          hash = "sha256-zCZFBLbG/3pdeBx6qBrVWy4deNNtbu4EK9RTL9wgTQQ=";
         };
       };
     in
